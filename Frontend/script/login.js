babc39f0cf0827d32b801f1c2a5401a37d1e486f
Sayed Iqra Hassan <iqrahsayed625@gmail.com>
2025-12-23 23:43:54 +0530

    Remove Firebase config from login.js
    Removed sensitive Firebase configuration details.

--- Frontend/script/login.js
+++ Frontend/script/login.js
@@ -1,11 +1,6 @@
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDtuYr4icwQf2HsvByrCZeqbEex28lL6GI",
-    authDomain: "genaihack-240d7.firebaseapp.com",
-    projectId: "genaihack-240d7",
-    storageBucket: "genaihack-240d7.firebasestorage.app",
-    messagingSenderId: "1095624251792",
-    appId: "1:1095624251792:web:8b4be21e68c1a8bcc2bb15"
+
 };
 
 firebase.initializeApp(firebaseConfig);
